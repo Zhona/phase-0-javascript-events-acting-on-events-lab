f6d0337de6762f1f7dd03bd87b84604eaae0953c
Teana Zhong <Teanawk@gmail.com>
2022-09-04 16:41:01 -0400

    commit tree 0187c3fb30367c9fb8bd265bf7cae8091d7dca75

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+/*
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "pink"
+
+dodger.style.bottom = "200px"
+
+const board = document.getElementById("game")
+board.style.backgroundColor = "lavender"
+*/
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+  function moveDodgerRight() {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+    
+      
+      if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+      }
+    }
+  
